2206eba37f1ac1221cba3f626ae5ffeaf3992e77
Ariel Debrouvier <ariel.debrouvier@gmail.com>
2019-04-24 02:06:23 -0300

    Fix zero crossings and add slope evaluation

--- src/main/java/ar/edu/itba/ati/ui/listeners/transformations/LaplacianListener.java
+++ src/main/java/ar/edu/itba/ati/ui/listeners/transformations/LaplacianListener.java
@@ -3,7 +3,9 @@ package ar.edu.itba.ati.ui.listeners.transformations;
 import ar.edu.itba.ati.image.Image;
 import ar.edu.itba.ati.ui.FrameHelper;
 import ar.edu.itba.ati.ui.WindowContext;
+import ar.edu.itba.ati.ui.dialogs.DoubleDialog;
 
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 
 public class LaplacianListener extends ATIActionListener {
@@ -15,7 +17,13 @@ public class LaplacianListener extends ATIActionListener {
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
         Image image = getWindowContext().getImageContainer().getImage();
-        Image newImage = image.laplacianOperator();
-        FrameHelper.create(newImage);
+
+        DoubleDialog dialog = new DoubleDialog("Threshold");
+        int result = JOptionPane.showConfirmDialog(null, dialog,
+                "Please enter slope threshold", JOptionPane.OK_CANCEL_OPTION);
+        if (result == JOptionPane.OK_OPTION) {
+            Image newImage = image.laplacianOperator(dialog.getDoubleValue().intValue());
+            FrameHelper.create(newImage);
+        }
     }
 }
---
+++ src/test/java/ar/edu/itba/ati/ZeroCrossingTest.java
@@ -0,0 +1,86 @@
+package ar.edu.itba.ati;
+
+import ar.edu.itba.ati.image.Constants;
+import ar.edu.itba.ati.image.GrayScalePixel;
+import ar.edu.itba.ati.image.Image;
+import ar.edu.itba.ati.image.Pixel;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
+
+public class ZeroCrossingTest {
+
+    private static int threshold = 0;
+
+    @Test
+    public void noChanges() {
+        List<Integer> values = Arrays.asList(-1, -1, -1, -1, -1, -1);
+        List<Pixel> pixels = values.stream().map(GrayScalePixel::new).collect(Collectors.toList());
+
+        Image image = new Image(3, 2, pixels, Image.Format.PGM);
+        Image zeroCrossing = image.zeroCrossing(threshold);
+
+        assertEquals(Constants.WHITE, zeroCrossing.getPixel(0, 0).getRed());
+        assertEquals(Constants.WHITE, zeroCrossing.getPixel(1, 0).getRed());
+        assertEquals(Constants.WHITE, zeroCrossing.getPixel(2, 0).getRed());
+        assertEquals(Constants.WHITE, zeroCrossing.getPixel(0, 1).getRed());
+        assertEquals(Constants.WHITE, zeroCrossing.getPixel(1, 1).getRed());
+        assertEquals(Constants.WHITE, zeroCrossing.getPixel(2, 1).getRed());
+    }
+
+    @Test
+    public void horizontalSignChange() {
+        List<Integer> values = Arrays.asList(1, -1, -1);
+        List<Pixel> pixels = values.stream().map(GrayScalePixel::new).collect(Collectors.toList());
+
+        Image image = new Image(3, 1, pixels, Image.Format.PGM);
+        Image zeroCrossing = image.zeroCrossing(threshold);
+
+        assertEquals(Constants.BLACK, zeroCrossing.getPixel(0, 0).getRed());
+        assertEquals(Constants.WHITE, zeroCrossing.getPixel(1, 0).getRed());
+        assertEquals(Constants.WHITE, zeroCrossing.getPixel(2, 0).getRed());
+    }
+
+    @Test
+    public void verticalSignChange() {
+        List<Integer> values = Arrays.asList(1, -1, -1);
+        List<Pixel> pixels = values.stream().map(GrayScalePixel::new).collect(Collectors.toList());
+
+        Image image = new Image(1, 3, pixels, Image.Format.PGM);
+        Image zeroCrossing = image.zeroCrossing(threshold);
+
+        assertEquals(Constants.BLACK, zeroCrossing.getPixel(0, 0).getRed());
+        assertEquals(Constants.WHITE, zeroCrossing.getPixel(0, 1).getRed());
+        assertEquals(Constants.WHITE, zeroCrossing.getPixel(0, 2).getRed());
+    }
+
+    @Test
+    public void horizontalZeroCrossing() {
+        List<Integer> values = Arrays.asList(1, 0, -1);
+        List<Pixel> pixels = values.stream().map(GrayScalePixel::new).collect(Collectors.toList());
+
+        Image image = new Image(3, 1, pixels, Image.Format.PGM);
+        Image zeroCrossing = image.zeroCrossing(threshold);
+
+        assertEquals(Constants.WHITE, zeroCrossing.getPixel(0, 0).getRed());
+        assertEquals(Constants.BLACK, zeroCrossing.getPixel(1, 0).getRed());
+        assertEquals(Constants.WHITE, zeroCrossing.getPixel(2, 0).getRed());
+    }
+
+    @Test
+    public void verticalZeroCrossing() {
+        List<Integer> values = Arrays.asList(1, 0, -1);
+        List<Pixel> pixels = values.stream().map(GrayScalePixel::new).collect(Collectors.toList());
+
+        Image image = new Image(1, 3, pixels, Image.Format.PGM);
+        Image zeroCrossing = image.zeroCrossing(threshold);
+
+        assertEquals(Constants.WHITE, zeroCrossing.getPixel(0, 0).getRed());
+        assertEquals(Constants.BLACK, zeroCrossing.getPixel(0, 1).getRed());
+        assertEquals(Constants.WHITE, zeroCrossing.getPixel(0, 2).getRed());
+    }
+}
