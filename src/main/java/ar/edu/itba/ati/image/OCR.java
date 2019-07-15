1a11a2c0c17c924a5c3e9a3ac8551ef18ac916b0
Ariel Debrouvier <ariel.debrouvier@gmail.com>
2019-07-14 22:43:05 -0300

    Modify ocr data load

--- src/main/java/ar/edu/itba/ati/image/OCR.java
+++ src/main/java/ar/edu/itba/ati/image/OCR.java
@@ -3,13 +3,17 @@ package ar.edu.itba.ati.image;
 import net.sourceforge.tess4j.ITesseract;
 import net.sourceforge.tess4j.Tesseract;
 import net.sourceforge.tess4j.TesseractException;
+import net.sourceforge.tess4j.util.LoadLibs;
+
+import java.io.File;
 
 public class OCR {
 
     public static void run(Image image) {
         ITesseract tesseract = new Tesseract();
 
-        tesseract.setDatapath("./tessdata");
+        File tessDataFolder = new File("tessdata");
+        tesseract.setDatapath(tessDataFolder.getAbsolutePath());
         tesseract.setLanguage("eng");
 
         try {
